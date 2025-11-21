cc2f6900f8c793c2bd64ec5ad612721f5f734475
BaBaKaM870 <yohan.dechamps131@gmail.com>
2025-11-21 15:38:13 +0100

    Add prompt before entering pyramid height
    Added a prompt message before reading the pyramid height.

--- pyramide.c
+++ pyramide.c
@@ -4,26 +4,24 @@ int main() {
     int n;
     int i, j;
 
-    // Saisie de la hauteur
+    // Afficher d’abord le message
     printf("Entrez la hauteur de la pyramide : ");
+
+    // Ensuite seulement lire l’entrée
     scanf("%d", &n);
 
     printf("\nPyramide de hauteur %d :\n\n", n);
 
-    // Boucle principale : chaque niveau de la pyramide
     for (i = 1; i <= n; i++) {
 
-        // 1) Affichage des espaces
         for (j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
-        // 2) Nombres croissants
         for (j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // 3) Nombres décroissants
         for (j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
