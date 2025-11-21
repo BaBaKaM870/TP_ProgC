06f3e871c36fff568efdfb6c1bd7577ad20d57fb
BaBaKaM870 <yohan.dechamps131@gmail.com>
2025-11-21 15:35:14 +0100

    Prompt user for pyramid height input
    Modified the pyramid program to prompt user for height input instead of using a fixed value.

--- pyramide.c
+++ pyramide.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int n = 5;   // hauteur de la pyramide
+    int n;
     int i, j;
 
-    printf("Pyramide de hauteur %d :\n\n", n);
+    // Saisie de la hauteur
+    printf("Entrez la hauteur de la pyramide : ");
+    scanf("%d", &n);
+
+    printf("\nPyramide de hauteur %d :\n\n", n);
 
     // Boucle principale : chaque niveau de la pyramide
     for (i = 1; i <= n; i++) {
 
-        // 1) Affichage des espaces (pour centrer)
+        // 1) Affichage des espaces
         for (j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
-        // 2) Nombres croissants : de 1 à i
+        // 2) Nombres croissants
         for (j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // 3) Nombres décroissants : de i-1 à 1
+        // 3) Nombres décroissants
         for (j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
 
-        // 4) Passage à la ligne suivante
         printf("\n");
     }
 
