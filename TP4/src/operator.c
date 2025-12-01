79e5a2181034500508d8e2b769ec41dc82382c9b
BaBaKaM870 <yohan.dechamps131@gmail.com>
2025-12-01 10:27:21 +0100

    Update operator.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,43 @@
+#include "operator.h"
+#include <stdio.h>
+
+int somme(int num1, int num2) {
+    return num1 + num2;
+}
+
+int difference(int num1, int num2) {
+    return num1 - num2;
+}
+
+int produit(int num1, int num2) {
+    return num1 * num2;
+}
+
+int quotient(int num1, int num2) {
+    if (num2 == 0) {
+        printf("Erreur : division par zero !\n");
+        return 0;
+    }
+    return num1 / num2;
+}
+
+int modulo(int num1, int num2) {
+    if (num2 == 0) {
+        printf("Erreur : modulo par zero !\n");
+        return 0;
+    }
+    return num1 % num2;
+}
+
+int et_binaire(int num1, int num2) {
+    return num1 & num2;
+}
+
+int ou_binaire(int num1, int num2) {
+    return num1 | num2;
+}
+
+int negation(int num1, int num2) {
+    (void)num2;  // évite un warning
+    return ~num1;
+}
